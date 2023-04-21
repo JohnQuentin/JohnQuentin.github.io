8d417fcad79d0430528c40033f9aed28e43f847e
JohnQuentin <67411883+JohnQuentin@users.noreply.github.com>
2023-04-21 08:42:27 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,36 @@
-$(document).ready(function() {
+// Sticky Header
+$(window).scroll(function() {
 
-  var curPage = 1;
-  var numOfPages = $(".skw-page").length;
-  var animTime = 1000;
-  var scrolling = false;
-  var pgPrefix = ".skw-page-";
-
-  function pagination() {
-    scrolling = true;
-
-    $(pgPrefix + curPage).removeClass("inactive").addClass("active");
-    $(pgPrefix + (curPage - 1)).addClass("inactive");
-    $(pgPrefix + (curPage + 1)).removeClass("active");
-
-    setTimeout(function() {
-      scrolling = false;
-    }, animTime);
-  };
-
-  function navigateUp() {
-    if (curPage === 1) return;
-    curPage--;
-    pagination();
-  };
-
-  function navigateDown() {
-    if (curPage === numOfPages) return;
-    curPage++;
-    pagination();
-  };
-
-  $(document).on("mousewheel DOMMouseScroll", function(e) {
-    if (scrolling) return;
-    if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
-      navigateUp();
-    } else { 
-      navigateDown();
+    if ($(window).scrollTop() > 100) {
+        $('.main_h').addClass('sticky');
+    } else {
+        $('.main_h').removeClass('sticky');
     }
-  });
+});
 
-  $(document).on("keydown", function(e) {
-    if (scrolling) return;
-    if (e.which === 38) {
-      navigateUp();
-    } else if (e.which === 40) {
-      navigateDown();
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.main_h').removeClass('open-nav');
+    } else {
+        $('.main_h').addClass('open-nav');
     }
-  });
+});
 
+$('.main_h li a').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_h').removeClass('open-nav');
+    }
+});
+
+// Navigation Scroll - ljepo radi materem
+$('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
 });
